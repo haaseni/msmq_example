630365e1057ff2ffb2fe9ab56c263ff7796bdf42
Nick <nicholas.haase@mutualofamerica.com>
2019-07-25 10:41:31 -0400

    Initial code push

---
+++ src/ConsoleReceiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Messaging;
+
+namespace ConsoleReceiver
+{
+    class Program
+    {
+        private const string QueuePath = @".\Private$\Test";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RECEIVER");
+
+            if (MessageQueue.Exists(QueuePath))
+            {
+                ReadMessages();
+            }
+
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+
+        static void ReadMessages()
+        {
+            using (var messageQueue = new MessageQueue(QueuePath))
+            {
+                var messages = messageQueue.GetAllMessages();
+
+                foreach (var message in messages)
+                {
+                    message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+                    Console.WriteLine($"\n -- Received Message: {message.Body}");
+                }
+
+                messageQueue.Purge();
+
+                Console.WriteLine("Press R to Refresh");
+                var response = Console.ReadKey();
+
+                if (response.KeyChar.ToString().ToUpper() == "R")
+                {
+                    ReadMessages();
+                }
+            }
+        }
+    }
+}
---
+++ src/ConsoleSender/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Messaging;
+
+namespace ConsoleSender
+{
+    class Program
+    {
+        private const string QueuePath = @".\Private$\Test";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SENDER");
+            SendMessage();
+        }
+
+        static void SendMessage()
+        {
+            MessageQueue messageQueue;
+
+            if (MessageQueue.Exists(QueuePath))
+            {
+                messageQueue = new MessageQueue(QueuePath)
+                {
+                    Label = "Testing Queue"
+                };
+            }
+            else
+            {
+                MessageQueue.Create(QueuePath);
+                messageQueue = new MessageQueue(QueuePath)
+                {
+                    Label = "Testing Queue"
+                };
+            }
+
+            Console.WriteLine("Enter message to send:");
+            var message = Console.ReadLine();
+
+            messageQueue.Send(message);
+
+            Console.WriteLine($"-- Sent {message}");
+
+            SendMessage();
+        }
+    }
+}
